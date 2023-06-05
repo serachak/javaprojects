da675e339a8c26f6ad3d2f7a9e5343bb115306f2
Sekhar Rachakonda <sekhar.rachakonda@cbre.com>
2023-06-05 18:42:25 -0500

    adding first java file.

---
+++ hello.java
@@ -0,0 +1,18 @@
+// Your First Program
+
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!  yahoo"); 
+        System.out.print("yes");
+        main2(args);
+	main3(args);
+    }
+    public static void main2(String[] args) {
+        System.out.println("Hello, World!  yahoo main2");
+        System.out.print("yes");
+    }
+    public static void main3(String[] args) {
+        System.out.println("Hello, World!  yahoo main 3");
+        System.out.print("yes");
+    }
+}
