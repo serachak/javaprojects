9cd4b1aa50d5b7c1c3d0ec3268fb1fa33d56bea6
cvrachak <cvrachak@gmail.com>
2023-09-03 11:32:48 -0500

    cleaned up the source code.

--- hello.java
+++ hello.java
@@ -4,17 +4,7 @@ class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!  yahoo"); 
         System.out.print("yes");
-        main2(args);
-	    main3(args);
         mainsamanvi(args);
-    }
-    public static void main2(String[] args) {
-        System.out.println("Hello, World!  yahoo main2");
-        System.out.print("yes");
-    }
-    public static void main3(String[] args) {
-        System.out.println("Hello, World!  yahoo main 3");
-        System.out.print("yes");
     }
     public static void mainsamanvi(String[] args) {
         System.out.println("Hello, World! yahoo mainsamanvi");
