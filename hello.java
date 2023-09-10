1ecadbf553c5ece63090b0171aa26ba8a4749e83
Kailllllllu <143355487+Kailllllllu@users.noreply.github.com>
2023-09-10 11:26:46 -0500

    Update hello.java
    added new method

--- hello.java
+++ hello.java
@@ -5,11 +5,17 @@ class HelloWorld {
         System.out.println("Hello, World!  yahoo"); 
         System.out.print("yes");
         mainsamanvi(args);
+        mainarka(args);
+        mainkailash(args);
     }
     public static void mainsamanvi(String[] args) {
         System.out.println("Hello, World! i am samanvi");
     }
     public static void mainarka(String[] args) {
         System.out.println("Hello, World! i am arka!");
+    }
+
+    public static void mainkailash(String[] args) {
+        System.out.println("Hello, World! i am kailash!");
     }
 }
